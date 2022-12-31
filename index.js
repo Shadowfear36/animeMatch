e1a52f73b7659477b27db76de8e203b052dbb82d
DylanRhinehart <dylanrhinehart@gmail.com>
2022-12-31 14:05:15 -0800

    generate-html-for-books-page

--- index.js
+++ index.js
@@ -1,55 +1,141 @@
 //render login page
-function displayLogin() {
+let currentUser = "testName";
 
-    //render logo/text
-    const headerText = document.createElement("h1");
-    headerText.textContent = "bookMatch";
-    headerText.className = "loginHeader";
+const getMainDiv = document.querySelector("#main");
 
-    //create login form
-    const loginForm = document.createElement("form");
-    const userNameInput = document.createElement("input");
-    const passwordInput = document.createElement("input");
-    const loginBtn = document.createElement("button");
+function renderApp() {
+    // renderLoginPage();
+    renderBookPage();
+}
 
-    //append inputs to login form
-    loginForm.appendChild(userNameInput);
-    loginForm.appendChild(passwordInput);
-    loginForm.appendChild(loginBtn);
+function renderLoginPage() {
+        //render logo/text
+        const headerText = document.createElement("h1");
+        headerText.textContent = "bookMatch";
+        headerText.className = "loginHeader";
+    
+        //create login form
+        const loginForm = document.createElement("form");
+        const userNameInput = document.createElement("input");
+        const passwordInput = document.createElement("input");
+        const loginBtn = document.createElement("button");
+        const signUpBtn = document.createElement("button");
+    
+        //append inputs to login form
+        loginForm.appendChild(userNameInput);
+        loginForm.appendChild(passwordInput);
+        loginForm.appendChild(loginBtn);
+        // loginForm.appendChild(signUpBtn);
+    
+        //form styles
+        loginForm.style.display = "block";
+        loginBtn.classList.add("centered");
+    
+        //user name input styles
+        userNameInput.type = "text";
+        userNameInput.placeholder = "Username";
+        userNameInput.id = "userNameInput";
+        userNameInput.required = true;
+        userNameInput.style.display = "block";
+        userNameInput.classList.add("centered");
+    
+        //password input styles
+        passwordInput.type = "text";
+        passwordInput.placeholder = "Password";
+        passwordInput.id = "passwordInput";
+        passwordInput.required = true;
+        passwordInput.style.display = "block";
+        passwordInput.classList.add("centered");
+    
+        //login btn styles
+        loginBtn.textContent = "Login";
+        loginBtn.type = "submit";
+        loginBtn.id = "loginBtn";
+        loginBtn.name = "formBtn";
+        loginBtn.value = "Login";
+        loginBtn.style.display = "block";
+        loginBtn.classList.add("centered");
+    
+        //sign up btn styles
+        // signUpBtn.textContent = "Sign up";
+        // signUpBtn.type = "submit";
+        // signUpBtn.id = "signUpBtn";
+        // signUpBtn.name = "formBtn";
+        // signUpBtn.value = "SignUp";
+        // signUpBtn.style.display = "block";
+        // signUpBtn.classList.add("centered");
+    
+        //append created items to Div
+        getMainDiv.appendChild(headerText);
+        getMainDiv.appendChild(loginForm);
+    
+        //<--EVENT LISTENERS-->
+        loginForm.addEventListener("submit", (e) => {
+            e.preventDefault();
 
-    //form styles
-    loginForm.style.display = "block";
+            // post to database at db.json
+            // const userName = userNameInput.value;
+            // const password = passwordInput.value;
+           
+            //set current user to current user input value
+            currentUser = userName
+            //render the book page
+            renderBookPage();
+        })
+}
 
-    //user name input styles
-    userNameInput.type = "text";
-    userNameInput.placeholder = "Username";
-    userNameInput.id = "userNameInput";
-    userNameInput.required = true;
-    userNameInput.style.display = "block";
+function renderBookPage() {
+    // remove html from previous page
+    getMainDiv.innerHTML = "";
 
-    //password input styles
-    passwordInput.type = "text";
-    passwordInput.placeholder = "Password";
-    passwordInput.id = "passwordInput";
-    passwordInput.required = true;
-    passwordInput.style.display = "block";
+    //top bar
+    const topBar = document.createElement("div");
+    const userH1 = document.createElement("h1");
+    const viewLikesBtn = document.createElement("button");
 
-    //login btn styles
-    loginBtn.textContent = "Login";
-    loginBtn.type = "submit";
-    loginBtn.className = "loginBtn";
-    loginBtn.id = "loginBtn";
-    loginBtn.style.display = "block";
+    //books
+    const bookDiv = document.createElement("div");
+    const bookImg = document.createElement("img");
+    const dislikeBtn = document.createElement("button");
+    const likeBtn = document.createElement("button");
+
+    // <---Top Bar-->
+    topBar.id = "topBar";
+    // append to topBar Div 
+    topBar.appendChild(userH1);
+    topBar.appendChild(viewLikesBtn);
+
+    userH1.innerText = currentUser;
+    viewLikesBtn.textContent = "View Likes";
+
+    //<--Books App -->
+    bookDiv.id = "bookDiv";
+    bookDiv.appendChild(bookImg);
+    bookDiv.appendChild(dislikeBtn);
+    bookDiv.appendChild(likeBtn);
+
+    bookImg.src = "";
+    dislikeBtn.textContent = "Dislike";
+    likeBtn.textContent = "Like";
 
     //append created items to Div
-    const getLoginDiv = document.querySelector("#login");
-    getLoginDiv.appendChild(headerText);
-    getLoginDiv.appendChild(loginForm);
 
+    //append to main div for rendering
+    getMainDiv.appendChild(topBar);
+    getMainDiv.appendChild(bookDiv);
+}
+
+function renderViewLikes(){
+    // remove html from previous page
+    getMainDiv.innerHTML = "";
+
+    // <---render html for View Likes Page-->
+    const topBar = document.createElement("div");
+    const userH1 = document.createElement("h1");
 }
 
 
 //Display After DOM Content Has Rendered
 document.addEventListener("DOMContentLoaded", () => {
-    displayLogin();
+    renderApp();
 });
